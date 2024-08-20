966ac20b4675226804a4f2a22ed4e865d14c8d06
shaaanuu <monshan50@gmail.com>
2024-08-20 14:24:11 +0530

    change: login button to download button

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,4 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+// Function to fetch the latest tag
+const getLatestTag = async () => {
+  try {
+    const response = await fetch(
+      "https://api.github.com/repos/shaaanuu/endernote-flutter/tags"
+    );
+    const tags = await response.json();
+    const latestTag = tags[0].name;
+    return `https://github.com/shaaanuu/endernote-flutter/releases/tag/${latestTag}`;
+  } catch (error) {
+    console.error("Error fetching the latest tag:", error);
+    return "#";
+  }
+};
+
 export const Navbar = () => {
+  const [tagUrl, setTagUrl] = useState("#");
+
+  useEffect(() => {
+    const fetchTagUrl = async () => {
+      const url = await getLatestTag();
+      setTagUrl(url);
+    };
+
+    fetchTagUrl();
+  }, []);
+
   return (
     <nav className="navbar">
       <a href="#">
@@ -7,12 +37,9 @@ export const Navbar = () => {
           <div className="name">Endernote</div>
         </div>
       </a>
-      <div className="accountInfo">
-        <a className="login" href="#">
-          Login
-        </a>
-        <a className="signup" href="#">
-          Sign Up
+      <div className="get-app">
+        <a className="download" href={tagUrl}>
+          Download
         </a>
       </div>
     </nav>
